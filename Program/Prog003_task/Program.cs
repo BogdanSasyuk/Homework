3f50fc3a392879c6f4ed61eb06b9c294f9808266
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-18 23:04:47 +0300

    Найти максимальное число

---
+++ Program/Prog003_task/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = 0;
+int B = 0;
+int C = 0;
+Console.Write("");
+A = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+B = Convert.ToInt32(Console.ReadLine());
+Console.Write("");
+C = Convert.ToInt32(Console.ReadLine());
+int max = A;
+    if(A > max) max = A;
+    if(B > max) max = B;
+    if(C > max) max = C;
+Console.WriteLine(max);
