afa9239002d4794f158161e4f10ff91780b97860
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-18 22:28:44 +0300

    Квадрат числа

--- Program/Prog001_task/Program.cs
+++ Program/Prog001_task/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = 0;
+int B = 0;
+Console.Write("Введите число А - ");
+A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В - ");
+B = Convert.ToInt32(Console.ReadLine());
+    if(B == A * A)
+    {
+        Console.WriteLine("Число В является квадратом числа А ");
+    }
+    else
+    {
+        Console.WriteLine("Число В не является квадратом числа А ");
+    }
