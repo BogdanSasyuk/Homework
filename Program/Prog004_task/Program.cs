b0e58b71c4fc76b6a1b33e6af5e74cfd6e997b05
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-18 23:37:53 +0300

    Проверка на четность

---
+++ Program/Prog004_task/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int A = 0;
+int n = 0;
+Console.Write("");
+A = Convert.ToInt32(Console.ReadLine());
+    if(n == A % 2)
+    {
+        Console.WriteLine("Введенное число - четное ");  
+    } 
+    else
+    {
+        Console.WriteLine("Введенное число нечетное ");
+    }
