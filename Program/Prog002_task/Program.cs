c7f553f686203cd67216ff784ce98fe3b3c99cf6
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-18 22:42:18 +0300

    Дни недели

---
+++ Program/Prog002_task/Program.cs
@@ -0,0 +1,19 @@
+﻿int N = 0;
+N = Convert.ToInt32(Console.ReadLine());
+    if(N == 1)
+       Console.WriteLine("Понедельник "); 
+    if(N == 2)
+       Console.WriteLine("Вторник ");
+    if(N == 3)
+       Console.WriteLine("Среда "); 
+    if(N == 4)
+       Console.WriteLine("Четверг "); 
+    if(N == 5)
+       Console.WriteLine("Пятница "); 
+    if(N == 6)
+       Console.WriteLine("Суббота "); 
+    if(N == 7)
+       Console.WriteLine("Воскресенье "); 
+    if(N > 7)
+       Console.WriteLine("В неделе всего 7 дней!!! "); 
+
