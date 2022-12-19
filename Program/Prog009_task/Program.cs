41c6ec547241293f340068cbaf6466105d779a52
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-20 02:20:22 +0300

    Проверка кратности и остаток

---
+++ Program/Prog009_task/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите первое число ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число ");
+int B = Convert.ToInt32(Console.ReadLine()); 
+
+int ost = A % B;
+    if(ost == 0)
+    {
+        Console.WriteLine("Число А кратно числу В ");
+    }
+    else
+    {
+        Console.WriteLine("Выводим остаток - "+ost % 10);
+    } 
+
