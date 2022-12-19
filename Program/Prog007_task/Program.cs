fb18d0a76acb6577b68096e1c690217ace57fc87
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-19 19:20:17 +0300

    Наибольшая цифра на отрезке

---
+++ Program/Prog007_task/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите двухзначное число ");
+int n = int.Parse(Console.ReadLine()!);
+     if(n >9 && n <100)
+     {
+          if(n / 10 > n % 10)
+             Console.WriteLine("Первая цифра больше ");   
+          else if(n / 10 == n % 10)
+             Console.WriteLine("Цифры одинаковы ");
+          else
+             Console.WriteLine("Bторая цифра больше ");
+     }  
+     else
+     {
+        Console.WriteLine("Число не является двузначным ");
+     }   
