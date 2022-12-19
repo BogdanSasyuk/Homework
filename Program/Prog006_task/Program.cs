10f42901c3e9c09c823cdf02aceeb033a63416ec
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-19 17:59:14 +0300

    Нахождение второй цифры в числе

---
+++ Program/Prog006_task/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите трехзначное число ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+
+  if(A > 99)
+  {
+        Console.WriteLine("Вторая цифра в числе  "+A.ToString()+" = "+A.ToString()[1]);
+  } 
+  
