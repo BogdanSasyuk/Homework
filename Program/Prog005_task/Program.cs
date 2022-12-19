60928ed1776c031a92a6cb08955c4c79bf07a76d
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-19 17:53:12 +0300

    Нахождение 3-ей цифры в числе

---
+++ Program/Prog005_task/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+  
+Console.WriteLine("Введите трехзначное число ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+
+  if(A > 99)
+  {
+        Console.WriteLine("Третья цифра в числе  "+A.ToString()+" = "+A.ToString()[2]);
+  } 
+  
+ 
