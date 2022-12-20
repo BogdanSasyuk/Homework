c4602f8c03d2cac285776dec1d57f8e595b1715b
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-20 23:52:49 +0300

    Принимает трехзначное число и на выходе второе

---
+++ Program/Prog010_task/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите три трехзначных числа ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+int B = Convert.ToInt32(Console.ReadLine()); 
+int C = Convert.ToInt32(Console.ReadLine()); 
+  if(A > 99 && A < 1000)
+  {
+        Console.WriteLine(A.ToString()+" => "+A.ToString()[1]);
+  } 
+  if(B > 99 && A < 1000)
+  {
+        Console.WriteLine(B.ToString()+" => "+B.ToString()[1]);
+  } 
+  if(C > 99 && A < 1000)
+  {
+        Console.WriteLine(C.ToString()+" => "+C.ToString()[1]);
+  } 
+
+ 
