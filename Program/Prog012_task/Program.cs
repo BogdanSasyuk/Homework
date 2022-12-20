a8b71d862301bf4e7572acd733c8ee3e3e19aa00
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-21 00:17:26 +0300

    Принимает цифру дня недели и проверяет будет ли выходной

---
+++ Program/Prog012_task/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите цифру соответствующую дню недели ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+int B = Convert.ToInt32(Console.ReadLine()); 
+int C = Convert.ToInt32(Console.ReadLine()); 
+  if(A == 6)
+  {
+        Console.WriteLine(A.ToString()+" ->  да ");
+  } 
+  if(B == 7)
+  {
+        Console.WriteLine(B.ToString()+" ->  да ");
+  } 
+  if(C == 1)                                              //(C > 0 && C < 6)
+  {
+        Console.WriteLine(C.ToString()+" ->  нет ");
+  } 
