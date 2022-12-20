4ef3fe387b6f6d50ae535526d2a983cbbdb52948
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-20 23:40:22 +0300

    Выводит третью цифру или сообщает что третьей цифры нет

---
+++ Program/Prog011_task/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Введите три числа ");
+int A = Convert.ToInt32(Console.ReadLine()); 
+int B = Convert.ToInt32(Console.ReadLine()); 
+int C = Convert.ToInt32(Console.ReadLine()); 
+ if(A > 99 && A < 1000)
+ {
+    Console.WriteLine(A.ToString()+"   -> "+A.ToString()[2]);
+ } 
+ if(B > 9 && B < 100)
+ {
+    Console.WriteLine(B.ToString()+"    -> Третьей цифры нет! ");
+ }
+      
+  
+ if(C > 9999 && C < 100000)
+ {
+    Console.WriteLine(C.ToString()+" -> "+C.ToString()[2]);
+ }
