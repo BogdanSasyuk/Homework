d02d9a25c8be3cdad24c781eacc573d15c3ffd7e
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-22 06:13:43 +0300

    Проверка, является ли число палиндромом

---
+++ Program/Prog019_task/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Введите пятизначное число для проверки, является ли оно палиндромом ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A.ToString()[0] != A.ToString()[4])
+{
+  Console.WriteLine(A.ToString()+" -> нет "); 
+}
+else
+if(A.ToString()[..4] == A.ToString()[..4])
+{
+  Console.WriteLine(A.ToString()+" -> да ");
+}    
+if(B.ToString()[0] != B.ToString()[4])
+{
+  Console.WriteLine(B.ToString()+" -> нет "); 
+}
+else
+if(B.ToString()[..4] == B.ToString()[..4])
+{
+  Console.WriteLine(B.ToString()+" -> да ");
+} 
+ if(C.ToString()[0] != C.ToString()[4])
+{
+  Console.WriteLine(A.ToString()+" -> нет "); 
+}
+else
+if(C.ToString()[..4] == C.ToString()[..4])
+{
+  Console.WriteLine(C.ToString()+" -> да ");
+} 
+  
