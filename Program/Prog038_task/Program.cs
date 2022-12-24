12edee98a929fff8fb625f589f294d5a5f10711a
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 20:53:11 +0300

    Разница между max и min элементов массива

---
+++ Program/Prog038_task/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 99);
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if(i == array.Length - 1)
+    {
+        Console.Write(array[i]+ "]     ");
+    }
+    else
+    {
+        Console.Write(array[i]+ ", ");
+    } 
+}
+int diff = 0;
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] < min)
+    {
+        min = array[i];        
+    }
+    else if(array[i] > max)
+    {
+        max = array[i];
+    }
+    diff = max - min;
+}
+Console.WriteLine("Разность между max и min -> " +diff);
