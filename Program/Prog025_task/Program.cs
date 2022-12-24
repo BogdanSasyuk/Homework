ed0c300726c8a87812cc5ca564962ee30a6d772d
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 05:40:26 +0300

    Возведение числа в степень

---
+++ Program/Prog025_task/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите число - ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа - ");
+int B = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+for (int i = 1; i <= B ; i++)
+{
+    num = num * A;
+}
+Console.WriteLine(A+ "^"+B+" -> "+num); 
