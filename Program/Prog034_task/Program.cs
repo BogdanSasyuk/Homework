199eea1e3e905250cb1240e57951c33dc744ad59
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 11:50:58 +0300

    Количество четных чисел в массиве

---
+++ Program/Prog034_task/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if(i == array.Length - 1)
+    {
+        Console.Write(array[i]+ "]     ");
+    }
+    else
+    {
+        Console.Write(array[i]+ ", ");
+    } 
+}
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+       
+        count = count + 1; 
+    }
+}
+Console.WriteLine("Четных элементов -> "+count);
