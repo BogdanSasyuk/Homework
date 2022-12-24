4c06711665757b1fdb1f7cf8d12670fa4e6831bb
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 12:44:55 +0300

    Сумма элементов на нечетных позициях

---
+++ Program/Prog036_task/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if(i == array.Length - 1)
+    {
+        Console.Write(array[i]+ "]     ");
+    }
+    else
+    {
+        Console.Write(array[i]+ ", ");
+    } 
+}
+
+int count = 0;
+for (int i = 1; i < array.Length; i = i + 2)
+{
+    count += array[i]; 
+}
+Console.WriteLine("Cумма элементов на нечетных позициях -> "+count);
