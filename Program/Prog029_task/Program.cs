63f1771285adfa474fbb5e9da153dd8c040e566b
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 06:03:12 +0300

    Вывод массива из случайных чисел

---
+++ Program/Prog029_task/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if(i == array.Length - 1)
+    {
+        Console.Write(array[i]+ ".    ");
+    }
+    else
+    {
+        Console.Write(array[i]+ ", ");
+    } 
+}
