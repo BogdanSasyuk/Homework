758841d364e8d89021e783f32a1621c7205c20db
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-24 23:29:19 +0300

    Произведение пар чисел в массиве

---
+++ Program/prog039_task/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+}
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    if(i == array.Length - 1)
+    {
+        Console.Write(array[i]+ "]     ");
+    }
+    else
+    {
+        Console.Write(array[i]+ ", ");
+    } 
+}
+int j = array.Length - 1;
+for ( int i = 0; i < (array.Length + 1) / 2; i++ )
+{
+    
+    Console.Write(array[i] * array[j]+ "   ");
+    j--;
+}
