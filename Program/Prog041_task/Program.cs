4acd71ffa435c61cbbef07533ea60cc98b1391d1
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-27 00:14:51 +0300

    Сколько чисел больше нуля

---
+++ Program/Prog041_task/Program.cs
@@ -0,0 +1,59 @@
+﻿
+Console.Write("Сколько чисел хотите ввести? - ");
+int numbers = int.Parse(Console.ReadLine());
+int[] array = new int[numbers];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"\nВведите число с индексом {i}: ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("\nВывод чисел и ->  количество чисел больше 0 \n");
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]+ ", ");
+    if(array[i] > 0) count = count + 1;
+}
+Console.Write( " ->  " +count+ "         \n "  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void FillArray(int[] collection);
+// {
+//     int size = collection.Length;
+//     int i = 0;
+//     while (i < size)
+//     {
+//         collection[i] = new Array[];
+//         i++;
+//     }
+// }
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int pos = 0;
+//     while (pos < count)
+//     {
+//         Console.Write(col[pos] + " ");
+//         pos++;
+//     }
+// }
+// Console.WriteLine("Введите ");
+// FillArray(array);
+// PrintArray(array);
