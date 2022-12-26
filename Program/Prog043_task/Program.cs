0667dd1e57e844e0b4495a337180b1ae6bdd51a6
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-27 00:49:52 +0300

    Нахождение точки пересечения двух прямых

---
+++ Program/Prog043_task/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+x = Math.Round(x, 1);
+y = Math.Round(y, 1);
+Console.WriteLine($"Пересечение прямых в точке: ({x}; {y})");
