caf542ab68769529626fc5a58ea608b18f732706
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-27 03:59:25 +0300

    Перевод из десятичной в двоичную

---
+++ Program/Prog044_task/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите число в десятичной системе: ");
+var decNum = int.Parse(Console.ReadLine());
+var binNum = DecToBin(decNum);
+
+string DecToBin(int decNum)
+{
+    var binNum = string.Empty;
+    while (decNum > 0)
+    {
+        binNum = (decNum % 2) + binNum;
+        decNum /= 2;
+    }
+    return binNum;
+}
+Console.WriteLine("{0} => {1}", decNum, binNum);
+   
