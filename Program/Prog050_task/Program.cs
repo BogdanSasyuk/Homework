2bf6211339eddd97c1b808ce1fd592cebe0fb82f
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-28 19:26:57 +0300

    Поиск числа в двумерном массиве

---
+++ Program/Prog050_task/Program.cs
@@ -0,0 +1,46 @@
+﻿
+Console.WriteLine("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine()); 
+double[,] array = new double[n,m];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,10);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+       Console.Write(" "+array[i,j] + "\t  "); 
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("\n Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isInArray = false;
+int count = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+       if(array[i,j] == number)
+       {     
+        isInArray = true;
+        count++;
+       }
+    }
+    
+}
+if(isInArray)
+{
+    Console.WriteLine($"Число {number} есть в массиве и встречается там {count} раз(а).");
+}
+else
+{
+    Console.WriteLine($"Числа {number} нет в массиве ");
+}
+
