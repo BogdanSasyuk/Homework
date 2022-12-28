51a55cb3972b983ec062c940c13f122434e7876a
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-29 01:46:23 +0300

    Показывает вторую цифру трехзначного числа

--- Program/Prog010_task/Program.cs
+++ Program/Prog010_task/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
 Console.WriteLine("Введите три трехзначных числа ");
 int A = Convert.ToInt32(Console.ReadLine()); 
 int B = Convert.ToInt32(Console.ReadLine()); 
