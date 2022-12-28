c2cc1c76f9f31421e62a0302b43e548762be963a
Bogdan Sasuyk <S9313384342@outlook.com>
2022-12-28 14:33:33 +0300

    Вывод двумерного массива с вещественными числами

---
+++ Program/prog047_task/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine()); 
+double[,] array = new double[n,m];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(-10,10) + new Random().NextDouble();
+        array[i,j] = Math.Round(array[i,j],1);
+    }
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+       Console.Write(" "+array[i,j] + "   "); 
+    }
+    Console.WriteLine();
+}
